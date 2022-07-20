5097632a5121de691b7d72723cb615838ffe0765
vulcan <vulcan@vesta.com>
2022-07-20 01:18:58 -0700

    Setup initial files. Implement kanji fetch request.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const API_URL = 'https://kanjialive-api.p.rapidapi.com/api/public/kanji/';
+const HEADERS = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com',
+    'X-RapidAPI-Key': '0d2f56666bmshb011cf5ae77339fp182053jsnb4d3b3653796'
+  }
+};
 
 function App() {
+  const [kanji, setKanji] = useState([]);
+
+  useEffect(() => {
+    searchSingleKanji('雨');
+  }, []);
+
+  const searchSingleKanji = async (k) => {
+    const response = await fetch(`${API_URL}${k}`, HEADERS);
+    const data = await response.json()
+    console.log(data)
+    setKanji(data.kanji);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello, world!</h1>
   );
-}
+};
 
 export default App;
