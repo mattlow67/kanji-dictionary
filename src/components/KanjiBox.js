4fb06c0e1d23cdb585c2b8d2b1086cdf7da7fec6
vulcan <vulcan@vesta.com>
2022-07-26 13:25:14 -0700

    Before changing layout.

--- src/components/KanjiBox.js
+++ src/components/KanjiBox.js
@@ -7,36 +7,45 @@ function KanjiBox({ kanjidata }) {
       { kanjidata.kanji ? (
 
         <>
+        <div className='ctr-row1'>
 
-          <img
-            src={kanjidata.kanji.video.poster ? kanjidata.kanji.video.poster : "https://via.placeholder.com/300"}
-            alt="No Image Found"
-          />
+            <img
+              src={kanjidata.kanji.video.poster ? kanjidata.kanji.video.poster : "https://via.placeholder.com/300"}
+              alt="No Image Found"
+            />
 
-          <p>
-            {/*Character: {kanjidata.kanji.character}
-            <br />*/}Meaning: {kanjidata.kanji.meaning.english}
-            <br />Kunyomi: {kanjidata.kanji.kunyomi.hiragana}
-            <br />Onyomi: {kanjidata.kanji.onyomi.katakana}
-          </p>
+            <div className='kanjifonts'>
+              <p>
+                {kanjidata.kanji.character}    {kanjidata.kanji.character}
+                <br />
+                {kanjidata.kanji.character}    {kanjidata.kanji.character}
+              </p>
+            </div>
 
-          <p>
-            Grade: {kanjidata.references.grade}
-            <br />Kodansha: {kanjidata.references.kodansha}
-            <br />Nelson: {kanjidata.references.classic_nelson}
-          </p>
+            <p>
+              {/*Character: {kanjidata.kanji.character}
+              <br />*/}Meaning: {kanjidata.kanji.meaning.english}
+              <br />Kunyomi: {kanjidata.kanji.kunyomi.hiragana}
+              <br />Onyomi: {kanjidata.kanji.onyomi.katakana}
 
-          <p>
-            Examples:
-            {(() => {
-              const examples = [];
-              for (let i = 0; i < kanjidata.examples.length && i < 6; i++) {
-                examples.push(<><br />{kanjidata.examples[i].japanese}</>);
-              }
-              return examples;
-            })()}
+              <br />Grade: {kanjidata.references.grade}
+              <br />Kodansha: {kanjidata.references.kodansha}
+              <br />Nelson: {kanjidata.references.classic_nelson}
+            </p>
 
-          </p>
+            <p>
+              Examples:
+              {(() => {
+                const examples = [];
+                for (let i = 0; i < kanjidata.examples.length && i < 6; i++) {
+                  examples.push(<><br />{kanjidata.examples[i].japanese}</>);
+                }
+                return examples;
+              })()}
+
+            </p>
+
+          </div>  {/*ctr-row1*/}
 
           <div className='kanjifonts'>
             <p>
