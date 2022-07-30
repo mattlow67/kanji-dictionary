bb9836659f17995319635ae9e9fb9d30150d3d28
vulcan <vulcan@vesta.com>
2022-07-30 14:30:20 -0700

    Add rel=noreferrer to links on App.js.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ An online dictionary and tool for researching and discovering Kanji.
 * Uses ReactJS Web framework
 * Tailors user experience by utilizing states
 * Interfaces with the [Kanji Alive](https://kanjialive.com/)'s public API
-* API's database contains data about hundreds of Kanji
+* API's database contains data on hundreds of Kanji
 
 ## To Do
 * Keep styling consistent between browsers
--- src/App.js
+++ src/App.js
@@ -52,10 +52,10 @@ function App() {
 
     <div className="footer">
       <p onClick={() => setOnHomePage(true)}>About</p>
-      <a href="https://github.com/mattlow67/kanji-search" target="_blank">
+      <a href="https://github.com/mattlow67/kanji-search" target="_blank" rel="noreferrer">
         <p>Source Code</p>
       </a>
-      <a href="https://kanjialive.com/" target="_blank">
+      <a href="https://kanjialive.com/" target="_blank" rel="noreferrer">
         <p>Kanji Alive</p>
       </a>
     </div>
